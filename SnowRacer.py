974e3cf6e4f227724f8f9cb37a276a6561e7f311
Arseniy.shrk <arseniy.shrk@gmail.com>
2026-01-06 16:32:22 +0700

    added collisions and background

--- SnowRacer.py
+++ SnowRacer.py
@@ -1,6 +1,6 @@
 import arcade
 
-SCALE = 2.0
+SCALE = 2
 TILE_WIDTH = 16
 SCREEN_WIDTH = 16 * TILE_WIDTH * SCALE
 SCREEN_HEIGHT = 20 * TILE_WIDTH * SCALE
@@ -32,7 +32,7 @@ class Racer(arcade.Sprite):
         self.center_x += dx
         self.center_y += dy
         self.speed += boost
-        self.center_x = max(self.width/2, min(SCREEN_WIDTH - self.width/2, self.center_x))
+
         self.center_y = max(self.height/2, min(SCREEN_HEIGHT - self.height/2, self.center_y))
 
 class SnowRacerGame(arcade.Window):
@@ -42,10 +42,22 @@ class SnowRacerGame(arcade.Window):
     def setup(self):
         self.racer_list = arcade.SpriteList()
 
+        tile_map = arcade.load_tilemap('trace.tmx', scaling=SCALE)
+        self.floar_list = tile_map.sprite_lists['floar']
+        self.shadow_list = tile_map.sprite_lists['shadows']
+        self.nature_list = tile_map.sprite_lists['nature']
+        self.tramplins = tile_map.sprite_lists['tramplins']
+        self.nets = tile_map.sprite_lists['nets']
+        self.barriers = tile_map.sprite_lists['barriers']
+        self.collision_list = tile_map.sprite_lists["collision"]
+
         self.racer = Racer(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 2 * TILE_WIDTH * SCALE)
         self.racer_list.append(self.racer)
 
         self.keys_pressed = set()
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+        self.racer, self.collision_list)
 
     def on_key_press(self, key, modifiers):
         self.keys_pressed.add(key)
@@ -57,11 +69,19 @@ class SnowRacerGame(arcade.Window):
     def on_update(self, delta_time):
         boost = 0.25
         self.racer_list.update(boost, delta_time, self.keys_pressed)
+        self.physics_engine.update()
 
     def on_draw(self):
         self.clear()
 
+        self.floar_list.draw()
+        self.shadow_list.draw()
+        self.nature_list.draw()
+        self.tramplins.draw()
+        self.nets.draw()
         self.racer_list.draw()
+        self.barriers.draw()
+        self.collision_list.draw()
 
 def main():
     game = SnowRacerGame()
